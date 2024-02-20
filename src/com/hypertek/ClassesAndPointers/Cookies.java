779d2ba7f118f036178441d15228e72b74cbcae8
CyberTekFA <Faiz.m.amiree@gmail.com>
2024-02-19 19:03:50 -0500

    add classes concept

---
+++ src/com/hypertek/ClassesAndPointers/Cookies.java
@@ -0,0 +1,16 @@
+package com.hypertek.ClassesAndPointers;
+
+public class Cookies {
+    private String Color;
+
+    public Cookies(String color) {
+        this.Color = color;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        var green = new Cookies("Green");
+
+
+    }
+}
